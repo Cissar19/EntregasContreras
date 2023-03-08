3877a91fa6279ee6882bf8387512d032c7936485
Cesar Contreras <cesar.contreras.rv@gmail.com>
2023-03-07 22:48:25 -0300

    se agrega metodo update, se logra que cambie en el txt pero no en la consola

--- Desafio_02/index.js
+++ Desafio_02/index.js
@@ -32,26 +32,48 @@ class ProductManager {
     }
     getProductsById = async (id) =>{
         let respuesta3 = await this.readProducts()
-        let filtro = respuesta3.find(product => product.id === id)
-        console.log(filtro)
+        if(!respuesta3.find(product => product.id === id)){
+            console.log("producto no encontrado")
+        }else{
+            console.log(respuesta3.find(product => product.id === id))
+        }
     }
+
     deleteProductById = async (id) =>{
         let respuesta3 = await this.readProducts()
         let productFilter = respuesta3.filter(products => products.id != id)
-        console.log(productFilter)
+        await fs.writeFile(this.patch, JSON.stringify(productFilter))
 
+    }
+    updateProducts = async({id, ...producto})=>{
+        await this.deleteProductById(id)
+        let produtOld = await this.readProducts();
+        let prodcutsModif = [{ ...producto, id }, ...produtOld];
+        await fs.writeFile(this.patch, JSON.stringify(prodcutsModif));
     }
 
 }
-
 const productos = new ProductManager()
- productos.addProduct("titulo1", "a1",100, "img1", "123", 5)
- productos.addProduct("titulo2", "a3",200, "img2", "222", 2)
- productos.addProduct("titulo3", "a4",200, "img2", "222", 2)
- productos.addProduct("titulo4", "a4",200, "img2", "222", 2)
- productos.addProduct("titulo5", "a5",200, "img2", "222", 2)
+productos.addProduct("titulo1", "a1",100, "img1", "123", 5)
+productos.addProduct("titulo2", "a3",200, "img2", "222", 2)
 
+
+productos.getProducts()
+
+
+productos.getProductsById(4)
+
+
+// productos.deleteProductById(1)
+
+productos.updateProducts({
+    title: 'titulo55',
+    description: 'a1',
+    price: 2200,
+    imagen: 'img1',
+    code: '123',
+    stock: 5,
+    id: 1
+})
 
 productos.getProductsById(1)
-
-productos.deleteProductById(1)
