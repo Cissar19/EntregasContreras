df648265f100edb63f87c686a159b7653214b240
Cesar Contreras <cesar.contreras.rv@gmail.com>
2023-03-13 16:02:41 -0300

    Fixes señalados por el tutor

--- Desafio_02/index.js
+++ Desafio_02/index.js
@@ -1,79 +1,74 @@
-import {promises as fs} from "fs";
+import { promises as fs } from "fs";
 
 class ProductManager {
-    constructor(){
-        this.patch = "./productos.txt"
-        this.products = []
-    }
-    static id = 0
+  constructor() {
+    this.path = "./productos.json";
+    this.products = [];
+  }
+  static id = 0;
 
-    addProduct = async (title,description,price,imagen,code,stock) => {
-        ProductManager.id++
-        let newProduct = {
-                title,
-                description,
-                price,
-                imagen,
-                code,
-                stock,
-                id: ProductManager.id
-            }
-        this.products.push(newProduct)
-        await fs.writeFile(this.patch, JSON.stringify(this.products))
-    }
-    readProducts = async() => {
-        let respuesta = await fs.readFile(this.patch, "utf-8")
-        return JSON.parse(respuesta)
-    }
+  addProduct = async (title, description, price, imagen, code, stock) => {
+    ProductManager.id++;
+    let newProduct = {
+      title,
+      description,
+      price,
+      imagen,
+      code,
+      stock,
+      id: ProductManager.id,
+    };
+    this.products.push(newProduct);
+    await fs.writeFile(this.path, JSON.stringify(this.products));
+  };
+  readProducts = async () => {
+    let products = await fs.readFile(this.path, "utf-8");
+    return JSON.parse(products);
+  };
 
-    getProducts = async() =>{
-        let respuesta2 = await this.readProducts()
-        console.log(respuesta2)
+  getProducts = async () => {
+    let products = await this.readProducts();
+    console.log(products);
+  };
+  getProductsById = async (id) => {
+    let products = await this.readProducts();
+    if (!products.find((product) => product.id === id)) {
+      console.log("producto no encontrado");
+    } else {
+      console.log(products.find((product) => product.id === id));
     }
-    getProductsById = async (id) =>{
-        let respuesta3 = await this.readProducts()
-        if(!respuesta3.find(product => product.id === id)){
-            console.log("producto no encontrado")
-        }else{
-            console.log(respuesta3.find(product => product.id === id))
-        }
-    }
+  };
 
-    deleteProductById = async (id) =>{
-        let respuesta3 = await this.readProducts()
-        let productFilter = respuesta3.filter(products => products.id != id)
-        await fs.writeFile(this.patch, JSON.stringify(productFilter))
-
-    }
-    updateProducts = async({id, ...producto})=>{
-        await this.deleteProductById(id)
-        let produtOld = await this.readProducts();
-        let prodcutsModif = [{ ...producto, id }, ...produtOld];
-        await fs.writeFile(this.patch, JSON.stringify(prodcutsModif));
-    }
-
+  deleteProductById = async (id) => {
+    let products = await this.readProducts();
+    let productFilter = products.filter((products) => products.id != id);
+    await fs.writeFile(this.path, JSON.stringify(productFilter));
+  };
+  updateProducts = async ({ id, ...producto }) => {
+    await this.deleteProductById(id);
+    let produtOld = await this.readProducts();
+    let prodcutsModif = [{ ...producto, id }, ...produtOld];
+    await fs.writeFile(this.path, JSON.stringify(prodcutsModif));
+  };
 }
-const productos = new ProductManager()
-productos.addProduct("titulo1", "a1",100, "img1", "123", 5)
-productos.addProduct("titulo2", "a3",200, "img2", "222", 2)
+const productos = new ProductManager();
+productos.addProduct("titulo1", "a1", 100, "img1", "123", 5);
+productos.addProduct("titulo2", "a3", 200, "img2", "222", 2);
 
+productos.getProducts();
 
-productos.getProducts()
-
-
-productos.getProductsById(4)
-
+productos.getProductsById(4);
 
 // productos.deleteProductById(1)
 
 productos.updateProducts({
-    title: 'titulo55',
-    description: 'a1',
-    price: 2200,
-    imagen: 'img1',
-    code: '123',
-    stock: 5,
-    id: 1
-})
+  title: "titulo55",
+  description: "a1",
+  price: 2200,
+  imagen: "img1",
+  code: "123",
+  stock: 5,
+  id: 1,
+});
 
-productos.getProductsById(1)
+productos.getProductsById(1);
