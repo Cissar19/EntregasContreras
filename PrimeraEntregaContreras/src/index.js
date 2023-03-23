8debaaa2a8d2926c4d7f4bcaa6dd27348ce546fc
Cesar Contreras <cesar.contreras.rv@gmail.com>
2023-03-23 18:18:35 -0300

    Se agrega Router

--- PrimeraEntregaContreras/src/index.js
+++ PrimeraEntregaContreras/src/index.js
@@ -1,37 +1,13 @@
-import express, { urlencoded } from "express";
-import ProductManager from "./controllers/ProductManager.js";
+import express from "express";
+import ProductRouter from "./router/product.routes.js";
 
 const app = express();
 const PORT = 8080;
-const product = new ProductManager();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/products", async (req, res) => {
-  res.send(await product.getProducts());
-});
-
-app.get("/products/:id", async (req, res) => {
-  let id = req.params.id;
-  res.send(await product.getProductsById(id));
-});
-
-app.post("/products", async (req, res) => {
-  let newProduct = req.body;
-  res.send(await product.addProducts(newProduct));
-});
-
-app.delete("/products/:id", async (req, res) => {
-  let id = req.params.id;
-  res.send(await product.deleteProducts(id));
-});
-
-app.put("/products/:id", async (req, res) => {
-  let id = req.params.id;
-  let updateProduct = req.body;
-  res.send(await product.updateProduct(id, updateProduct));
-});
+app.use("/products", ProductRouter);
 
 app.listen(PORT, () => {
   console.log("Estamos arriba papu");
